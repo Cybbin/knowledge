dfd6863e2ecd35a9b044b3cf185e16dfc06a1223
chalice <chalice,chen@oneplus.com>
2020-05-20 18:46:59 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  base: '/knowledge/',
+  dest: 'dist',
+  title: '知识库',
+  themeConfig: {
+    repo: 'https://github.com/Cybbin/knowledge',
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdated: '上次更新',
+    nav: [
+      // {
+      //   text: 'text',
+      //   link: '/internet/'
+      // }
+    ],
+    sidebar: {
+      '/internet/': [
+        {
+          title: '网络',
+          collapsable: false,
+          children: [
+            // ['prepare/', 'Introduction'],
+            'http'
+          ]
+        }
+      ]
+    }
+  }
+}
